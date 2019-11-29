3bd71ed139c96a054b3583a5056023f381825f2c
mattAAA <mashaver@CLT-MD-90FUF8J9.local>
2019-11-29 12:34:55 -0500

    Updated Storybook.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { GlobalStyle } from '../src/shared/global';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { GlobalStyle } from "../src/shared/global";
 
+addDecorator(withA11y);
 addDecorator(story => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
+    <>
+        <GlobalStyle />
+        {story()}
+    </>
 ));
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+configure(require.context("../src", true, /\.stories\.js$/), module);
