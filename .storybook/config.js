b8217674624c9bc12382fa3d557583d974522842
mattAAA <mashaver@CLT-MD-90FUF8J9.local>
2019-11-29 13:21:20 -0500

    Added markup content.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
 import { GlobalStyle } from "../src/shared/global";
 
 addDecorator(withA11y);
@@ -10,5 +11,10 @@ addDecorator(story => (
     </>
 ));
 
-// automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+configure(
+    [
+        require.context("../src", false, /Intro\.stories\.mdx/),
+        require.context("../src", true, /\.stories\.(js|mdx)$/)
+    ],
+    module
+);
